fa87bf75ba4bd84a0dd62248e4c4cf925a95a414
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-04 13:59:26 +0200

    h

--- forEach.js
+++ forEach.js
@@ -1,8 +1,21 @@
-const n = [1, 2, 3, 4, 5, 6];
+// const letter = ['a','d','s','f','h','y','d','a'];
 
-n.forEach(consoleItem);
+// let count= {}
+// letter.forEach(item => {
+//     if(count[item]){
+//         count[item]++;
+//     }else{
+//         count[item] = 1;
+//     }
+// })
 
-function consoleItem(item, index, arr) {
-    console.log('a[' + index + '] =' + item, arr);
-}
+// console.log(count)
+
+const n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+const double = n.map((value, index, arr) => {
+    return value * 2
+})
+
+console.log(double)
 
