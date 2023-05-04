ab5bfed77ccd09fa3d22104938f31270c4b7a0f5
Olimjon Orifjonov <98266046+olimjon1606@users.noreply.github.com>
2023-05-04 20:28:28 +0200

    Update forEach.js

--- forEach.js
+++ forEach.js
@@ -1,102 +1,112 @@
-// const letter = ['a','d','s','f','h','y','d','a'];
+// // const letter = ['a','d','s','f','h','y','d','a'];
 
-// let count= {}
-// letter.forEach(item => {
-//     if(count[item]){
-//         count[item]++;
-//     }else{
-//         count[item] = 1;
-//     }
+// // let count= {}
+// // letter.forEach(item => {
+// //     if(count[item]){
+// //         count[item]++;
+// //     }else{
+// //         count[item] = 1;
+// //     }
+// // })
+
+// // console.log(count)
+
+// const n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+
+// const double = n.map((value, index, arr) => {
+//     return value * 2
 // })
 
-// console.log(count)
+// console.log(double)
 
-const n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+// // Map 
+// const products = [
+//     {
+//         name: ' laptop',
+//         price: 1000,
+//         count: 5
+//     },
+//     {
+//         name: ' desktop',
+//         price: 1500,
+//         count: 3
+//     },
+//     {
+//         name: 'phone',
+//         price: 800,
+//         count: 10
+//     }
+// ]
 
-const double = n.map((value, index, arr) => {
-    return value * 2
-})
+// const totalProductValue = products.map(item => item.price * item.count)
+// console.log(totalProductValue)
 
-console.log(double)
+// // filter array 
 
-// Map 
-const products = [
-    {
-        name: ' laptop',
-        price: 1000,
-        count: 5
-    },
-    {
-        name: ' desktop',
-        price: 1500,
-        count: 3
-    },
-    {
-        name: 'phone',
-        price: 800,
-        count: 10
-    }
-]
+// let numbers = [1, 2, 100, 3, 4, 5]
+// const even = numbers.filter(isEven);
 
-const totalProductValue = products.map(item => item.price * item.count)
-console.log(totalProductValue)
+// function isEven(value) {
+//     return value % 2 === 0
+// }
+// console.log(even)
 
-// filter array 
+// // reduce array 
+// const total = numbers.reduce(sum, 0)
+// function sum(accumulator, value) {
+//     return accumulator + value
+// }
+// console.log(total)
 
-let numbers = [1, 2, 100, 3, 4, 5]
-const even = numbers.filter(isEven);
+// const max = numbers.reduce(callBack, -Infinity)
 
-function isEven(value) {
-    return value % 2 === 0
+// function callBack(accumulator, value) {
+//     if (accumulator > value) {
+//         return accumulator
+//     } else {
+//         return value
+//     }
+
+// }
+// console.log(max)
+
+// // slice array 
+
+// const number2 = numbers.slice(3)
+// console.log(number2)
+
+
+// // splice array 
+
+// numbers.splice(2, 3)
+
+// // sorting 
+
+// const ns = [23, 34, 1, 4, 364, 35, 556, 67, 3]
+// ns.sort(compare)
+// function compare(a, b) {
+//     return a - b;
+// }
+// console.log(ns)
+
+
+// // concat array 
+// const a = [1, 2, 3]
+// const b = [4, 5, 6]
+// const d = [7, 8, 9]
+
+// const c = a.concat(b)
+// console.log(c)
+// const f = a.concat(b,d,c)
+// console.log(f)
+
+
+
+const letters = ['a', 'b', 'c', 'd', 'e', 'f']
+function log(element, index) {
+    console.log(this)
 }
-console.log(even)
-
-// reduce array 
-const total = numbers.reduce(sum, 0)
-function sum(accumulator, value) {
-    return accumulator + value
+const obj = {
+    name: 'value'
 }
-console.log(total)
-
-const max = numbers.reduce(callBack, -Infinity)
-
-function callBack(accumulator, value) {
-    if (accumulator > value) {
-        return accumulator
-    } else {
-        return value
-    }
-
-}
-console.log(max)
-
-// slice array 
-
-const number2 = numbers.slice(3)
-console.log(number2)
-
-
-// splice array 
-
-numbers.splice(2, 3)
-
-// sorting 
-
-const ns = [23, 34, 1, 4, 364, 35, 556, 67, 3]
-ns.sort(compare)
-function compare(a, b) {
-    return a - b;
-}
-console.log(ns)
-
-
-// concat array 
-const a = [1, 2, 3]
-const b = [4, 5, 6]
-const d = [7, 8, 9]
-
-const c = a.concat(b)
-console.log(c)
-const f = a.concat(b,d,c)
-console.log(f)
-
+letters.forEach(log, obj)
